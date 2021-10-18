37a8c2d4f93150af3b40c44eac0ffc711415be75
jose causil <jmcausil@uniguajira.edu.co>
2021-10-17 19:18:15 -0500

    par

---
+++ clase1.py
@@ -0,0 +1,43 @@
+# TRIANGULO
+
+# def calcularArea(base, altura): 
+#     areatriangulo = base * altura / 2 
+#     return areatriangulo
+
+# def main():
+#     base = int(input ("Digite la base del triangulo "))
+#     altura = int(input("Digite la altura del triangulo "))
+#     area = calcularArea (base, altura) 
+#     print (area)
+
+# --------------------------------------------------
+
+def capturarNumero():
+    numero = int(input("ingrese el numero: "))
+    if numero%2==0:
+        print (f"el numero {numero} es par")
+    else: 
+        print("el numero es impar")
+    
+
+
+
+def main():
+    capturarNumero()
+
+
+if __name__== "__main__": #inicio del programa 
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
