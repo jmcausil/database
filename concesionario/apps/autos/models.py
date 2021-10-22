71d5467ec253413e3c285e865c22cd56d1cce36b
jose causil <jmcausil@uniguajira.edu.co>
2021-10-21 22:18:33 -0500

    OK

--- concesionario/apps/autos/models.py
+++ concesionario/apps/autos/models.py
@@ -1,13 +1,16 @@
 from django.db import models
-from apps.mecanicos.models import Mecanicos
+# from apps.mecanicos.models import Mecanicos
 from concesionario.apps.clientes.models import Clientes
-from concesionario.apps.mecanicos.models import Mecanicos
+# from concesionario.apps.mecanicos.models import Mecanicos
+
+
+
 # Create your models here.
 class Autos(models.Model):
     matricula = models.CharField(max_length=100)
     modelo = models.CharField(max_length=100)
     marca = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
-    mecanicos = models.ManyToManyField(Mecanicos)
+    # mecanicos = models.ManyToManyField(Mecanicos)
     Clientes = models.ForeignKey(Clientes)
    
